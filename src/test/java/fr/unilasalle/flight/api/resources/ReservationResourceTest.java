dadb7dc63e55b70cf3bd033abace0db4e2db3b1d
Guigui0812 <guillaume.rohee0812@gmail.com>
2023-12-05 15:37:37 +0100

    Unit Test for Reservation OK

--- src/test/java/fr/unilasalle/flight/api/resources/ReservationResourceTest.java
+++ src/test/java/fr/unilasalle/flight/api/resources/ReservationResourceTest.java
@@ -26,24 +26,43 @@ public class ReservationResourceTest {
                 .when().get("/reservations?flightNumber=AF101")
                 .then()
                 .statusCode(200)
-                .body("size()", equalTo(2));
+                .body("size()", equalTo(3));
     }
 
     @Test
     public void createReservation() {
+        String jsonBody = "{\n" +
+                "  \"flight\": {\n" +
+                "    \"id\": 1,\n" +
+                "    \"number\": \"AF101\",\n" +
+                "    \"origin\": \"Paris\",\n" +
+                "    \"destination\": \"New York\",\n" +
+                "    \"departureDate\": \"2023-12-01\",\n" +
+                "    \"departureTime\": \"07:00:00\",\n" +
+                "    \"arrivalDate\": \"2023-12-01\",\n" +
+                "    \"arrivalTime\": \"10:00:00\",\n" +
+                "    \"plane\": {\n" +
+                "      \"id\": 1,\n" +
+                "      \"operator\": \"AirbusIndustrie\",\n" +
+                "      \"model\": \"AIRBUS A380\",\n" +
+                "      \"registration\": \"F-ABCD\",\n" +
+                "      \"capacity\": 10\n" +
+                "    }\n" +
+                "  },\n" +
+                "  \"passenger\": {\n" +
+                "    \"id\": 5,\n" +
+                "    \"firstName\": \"test7\",\n" +
+                "    \"surname\": \"ttt\",\n" +
+                "    \"email\": \"test2.ttt@gmail.com\"\n" +
+                "  }\n" +
+                "}";
+
         given()
-                .when()
-                .body("{\n" +
-                        "    \"flightNumber\": \"AF124\",\n" +
-                        "    \"passenger\": {\n" +
-                        "        \"id\": 1\n" +
-                        "    }\n" +
-                        "}")
+                .body(jsonBody)
                 .contentType("application/json")
                 .post("/reservations")
                 .then()
-                .statusCode(201)
-                .body(equalTo("Reservation created"));
+                .statusCode(201);
     }
 
     // Tester le cas où le passager n'existe pas (où il est créé automatiquement)
